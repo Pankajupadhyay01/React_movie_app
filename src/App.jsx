507fccf074c2f4fdea237f24b8f25818eae203b0
pankaj upadhyay <pankajup.git01@gmail.com>
2023-07-31 22:58:33 +0530

    detail page updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { React, useState, Suspense, lazy } from 'react'
 import './App.css'
 const Navbar = lazy(() => import("./Component/Navbar"));
-const Home = lazy(() => import("./Component/Home"));
+import Home from './Component/Home'; 
 const Search = lazy(() => import("./Component/Search"));
 const Trending = lazy(() => import("./Component/Trending"));
 import {
@@ -19,7 +19,8 @@ function App() {
       <div className="App">
         <Navbar />
         <Routes>
-          <Route exact path='/' element={<Suspense fallback={<div className='flex justify-center items-center m-auto'> <Loading/> </div>}> <Home /></Suspense>}></Route>
+          <Route exact path='/' element={<Home/>}></Route> 
+
           <Route exact path='/Search' element={<Suspense fallback={<div className='flex justify-center items-center m-auto'> <Loading/> </div>}><Search /></Suspense>}></Route>
           <Route exact path='/trending' element={<Suspense fallback={<div className='flex justify-center items-center m-auto'> <Loading/> </div>}><Trending /></Suspense>}></Route>
           <Route exact path='/detail/:data' element={<Suspense fallback={<div className='flex justify-center items-center m-auto'> <Loading/> </div>}><Detail /></Suspense>}></Route>
--- src/Component/Detail.jsx
+++ src/Component/Detail.jsx
@@ -40,7 +40,7 @@ const Detail = () => {
           <div>
             <div className='m-auto w-full text-center justify-center items-center text-5xl text-blue-500'>{post.name}</div>
             <div className='my-[10px] flex md:flex-row flex-col m-auto w-[90%] md:justify-between'>
-              <div className="w-full md:w-[50%] h-[50vh] flex">
+              <div className="w-full md:w-[50%] h-[90vh] flex">
                 <img src={`https://image.tmdb.org/t/p/w1280${post.poster_path}`} className='w-full h-full' alt="" />
               </div>
               <div className='w-full md:w-[40%] flex justify-center flex-col space-y-3 text-lg font-blue-400'>
@@ -56,10 +56,10 @@ const Detail = () => {
                 <div>
                   {func()}
                 </div>
-                <div className='flex '>
+                <div className='flex flex-wrap '>
                   {
                     genres.map((pro, i) => (
-                      <p className='bg-blue-700 mx-[10px] py-[5px] px-3 rounded-full text-white'>{pro.name}</p>
+                      <p className='bg-blue-700 my-2 mx-[10px] py-[5px] px-3 rounded-full text-white'>{pro.name}</p>
                     ))
                   }
                 </div>
